e11d76c59ccbd0eb8f35c5164578abdcef657ae0
shl68 <105649474+shl68@users.noreply.github.com>
2023-11-23 00:40:28 +0900

    Add files via upload

---
+++ Week8_Sorter/IntData.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class IntData implements SortInfce{
+    public int[] arr;
+    public IntData(){
+        arr= new int[10];
+        Random random =new Random();
+        for(int i=0;i<10;i++){
+            arr[i]=random.nextInt(0,100);
+        }
+    }
+    public static void main(String[] a){
+        IntData i =new IntData();
+        System.out.println(Sorter.bubleSort(i, 0,i.arr.length-1));
+    }
+}
---
+++ Week8_Sorter/TesterMain.java
@@ -0,0 +1,10 @@
+// public class TestterMain{
+    
+//         IntData data = new IntData();
+//         Sorter sorter = new Sorter();
+//         data.printData();
+//         sorter.bubleSort(data, 0, data.getSize()-1);
+//         System.out.println("---------");
+//         data.printData();
+//     }
+// }
